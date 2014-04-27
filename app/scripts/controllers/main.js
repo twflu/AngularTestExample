e7b2e1f8d5259a5779eb7baf96b617629c15a310
twflu <yzzhou@thoughtworks.com>
2014-04-27 23:20:27 +0800

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('yeomanTestApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
